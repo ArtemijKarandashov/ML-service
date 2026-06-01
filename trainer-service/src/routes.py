26b961b8a7de132a4c58411954cb95970cb8ad4f
ArtemijKarandashov <artemij.karandashov@yandex.ru>
2026-06-01 17:55:02 +0300

    docker

--- trainer-service/src/routes.py
+++ trainer-service/src/routes.py
@@ -1,19 +1,67 @@
 from fastapi import APIRouter, Request, UploadFile, Depends
 from fastapi.responses import HTMLResponse, FileResponse, JSONResponse
-from fastapi.templating import Jinja2Templates
 from fastapi.exceptions import HTTPException
+from starlette.datastructures import Headers
 
 from sqlmodel.ext.asyncio.session import AsyncSession
+from typing import List
+from pydantic import BaseModel
 
-import os
-import uuid
+import io
 
 from .config import Config
+from .services import storage_service
 
 trainer_router = APIRouter()
 
 @trainer_router.post("/train")
 async def train(
-    uid: str
+    csv_file: UploadFile
 ):
-    pass
+    try:
+        # Scary? I dunno
+        content = await csv_file.read()
+
+        # do model call here with async. Resulting .pkl meant to be saved in file_bytes variable.
+        filename = "PLACEHOLDER"
+        file_bytes = b"PLACEHOLDER"
+        file_stream = io.BytesIO(file_bytes)
+        upload_file = UploadFile(
+            file=file_stream, 
+            filename=filename,
+            headers=Headers({"content-type": "text/plain"}),
+            size=len(file_bytes)
+        )
+
+        # saving .pkl model on storage server
+        response = await storage_service.save_pkl_file(file=upload_file)
+        response_data = response.json()
+        json_data = {
+            "message": "Model successfully trained",
+            "uid": response_data["uid"]
+        }
+        return JSONResponse(content=json_data, status_code=200)
+    except:
+        pass
+
+# FOR PREDICTOR
+
+predictor_router = APIRouter()
+
+@predictor_router.post("/predict")
+async def predict(
+    data: List[BaseModel]
+):
+    try:
+        ...
+    except:
+        pass
+
+@predictor_router.post("/predict_csv")
+async def predict_csv(
+    csv_file: UploadFile
+):
+    try:
+        ...
+    except:
+        pass
