0c8c93d4984bc0f4ca5868a593f012d20fb33b51
AntSib <antonsibilev2005@gmail.com>
2026-06-19 12:52:04 +0300

    Added CI

---
+++ trainer-service/src/test_model_creation.py
@@ -0,0 +1,19 @@
+from ml_pipeline.pipeline_manager import (
+    TrainingPipeline,
+)
+
+if __name__ == "__main__":
+    with open("ml_pipeline/data/loan_data.csv", "rb") as file:
+        train_csv_bytes = file.read()
+
+    trainer = TrainingPipeline(train_csv_bytes)
+    trainer.run_pipeline()
+
+    model_bytes = trainer.get_model_bytes()
+    metrics = trainer.get_metrics()
+
+    with open("dist/model.pkl", "wb") as model_file:
+        file.write(model_file)
+
+    with open("dist/metrics.txt", "w") as metrics_file:
+        file.write(metrics_file)
